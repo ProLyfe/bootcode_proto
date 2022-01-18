4dff3ba668b32cfb97eaf459f936a5843974e55a
ProLyfe <rayane.prolyfe@gmail.com>
2022-01-18 23:46:17 +0100

    [remove] bootcode commu

--- pages/communitybootcode/commubootcode.js
+++
@@ -1,7 +0,0 @@
-import BootCodeComponent from "../../components/BootCodeCommponent";
-
-const CommuBootCode = ({ html, css }) => (
-    <BootCodeComponent  html={html} css={css}/>
-);
-
-export default CommuBootCode;
